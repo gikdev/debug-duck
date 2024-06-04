4a9badbda6ca5cd382eca1adbf82159caa9915ab
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-06-04 08:29:10 +0330

    Did a couple of UI adjustments

--- src/components/EmptyCard.jsx
+++ src/components/EmptyCard.jsx
@@ -1,6 +1,6 @@
 export default function EmptyCard() {
   return (
-    <div className="card card-bordered shadow-xl max-w-96 text-center bg-transparent mx-auto w-full">
+    <div className="card card-bordered shadow-xl max-w-80 text-center bg-base-200 my-10 mx-auto w-full">
       <figure>
         <img src="https://wd-bahrami.storage.iran.liara.space/images/empty-box-and-fly.png" alt="" />
       </figure>
